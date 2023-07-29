e16d7dfe684aa3e4fce9da7b3a1bf46130168414
George Peshkov <jorahzo@Georges-MacBook-Pro.local>
2023-07-29 11:18:05 -0400

    Init. with HelloWorld program.

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by George Peshkov on 7/29/23.
+//
+
+#include <stdio.h> // header file for IO
+
+int main() { // main function
+    printf("Hello, World!\n"); // print message
+    return 0; // function returns 0
+}
